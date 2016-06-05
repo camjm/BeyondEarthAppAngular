c5094ef4aedfa85277129315e3679f993378eafe
Campbell Mackay <Campbell Mackay>
2016-06-06 11:20:10 +1200

    added comments

--- js/directives/gameInfo.js
+++ js/directives/gameInfo.js
@@ -1,11 +1,17 @@
+//Directives help Readability (expressive HTML that abstract the app's behaviour) and Reusability (self-contained units of functionality to avoid repetition)
+// This directive teaches the browser a new, self-contained, interactive HTML element: <game-info>
+
 app.directive('gameInfo', function() {
     return {
-        restrict: 'E',
-        scope: {
-            info: '='
+        restrict: 'E',    // How the directive will be used in the view: this directive is a new HTML element
+        scope: {          // How information will be passed to the directive
+            info: '='     // via an attribute named 'info' on the <game-info> element
         },
-        templateUrl: 'js/directives/gameInfo.html',
-        link: function(scope, element, attrs) {
+        templateUrl: 'js/directives/gameInfo.html',   // specifies the HTML to use to display the data
+        link: function(scope, element, attrs) {       // used to create interactive directives that respond to user actions
+            // scope: the directive's scope ('info' will be a property on this object) and new properties can be added for use in the template
+            // element: refers to the directive's HTML element
+            // attrs: contains the element's attributes
             scope.incrementCity = function(adjustment) {
                 scope.info.cityCount += adjustment;
             };
