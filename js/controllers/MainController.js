d673215ac6cf7efd535fddee8eeba20c178c5fce
Campbell Mackay <Campbell Mackay>
2016-06-06 10:05:28 +1200

    fixed bindings and formatted code

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,89 +1,54 @@
 // A controller manages the app's data.
 
 app.controller('MainController', ['$scope', function($scope) {
-  $scope.title = 'My Saved Games';
-  $scope.games = [
-    {
-      description: 'My first game',
-      faction: 'Polystralia',
-      status: 'In Progress',
-      startdate: new Date('2014', '03', '08'),
-      cityCount: 13,
-      technologyCount: 19,
-      affinities: {
-        purity: 2,
-        supremacy: 3,
-        harmony: 7
-      }
-    },
-    {
-      description: 'My best game',
-      faction: 'ARC',
-      status: 'In Progress',
-      startdate: new Date('2013', '08', '01'),
-      cityCount: 33,
-      technologyCount: 8,
-      affinities: {
-        purity: 2,
-        supremacy: 3,
-        harmony: 7
-      }
-    },
-    {
-      description: 'A very bad game',
-      faction: 'Pan Asian Cooperative',
-      status: 'In Progress',
-      startdate: new Date('2013', '08', '01'),
-      cityCount: 6,
-      technologyCount: 10,
-      affinities: {
-        purity: 2,
-        supremacy: 3,
-        harmony: 7
-      }
-    },
-    {
-      description: 'I dont know',
-      faction: 'Chungsu',
-      status: 'In Progress',
-      startdate: new Date('2013', '08', '01'),
-      cityCount: 12,
-      technologyCount: 11,
-      affinities: {
-        purity: 2,
-        supremacy: 3,
-        harmony: 7
-      }
-    }
-  ];
-  $scope.plusOneCity = function(index) {
-    $scope.games[index].cityCount += 1;
-  };
-  $scope.minusOneCity = function(index) {
-    $scope.games[index].cityCount -=1;
-  };
-  $scope.plusOneTechnology = function(index) {
-    $scope.games[index].technologyCount += 1;
-  };
-  $scope.minusOneTechnology = function(index) {
-    $scope.games[index].technologyCount -=1;
-  };
-  $scope.plusOnePurity = function(index) {
-    $scope.games[index].affinities.purity += 1;
-  };
-  $scope.minusOnePurity = function(index) {
-    $scope.games[index].affinities.purity -=1;
-  };
-  $scope.plusOneSupremacy = function(index) {
-    $scope.games[index].affinities.supremacy += 1;
-  };
-  $scope.minusOneSupremacy = function(index) {
-    $scope.games[index].affinities.supremacy -=1;
-  };
-  $scope.plusOneHarmony = function(index) {
-    $scope.games[index].affinities.harmony += 1;
-  };
-  $scope.minusOneHarmony = function(index) {
-    $scope.games[index].affinities.harmony -=1;
-  };
+    $scope.title = 'My Saved Games';
+    $scope.games = [{
+        description: 'My first game',
+        faction: 'Polystralia',
+        status: 'In Progress',
+        startdate: new Date('2014', '03', '08'),
+        cityCount: 13,
+        technologyCount: 19,
+        affinities: {
+            purity: 2,
+            supremacy: 3,
+            harmony: 7
+        }
+    }, {
+        description: 'My best game',
+        faction: 'ARC',
+        status: 'In Progress',
+        startdate: new Date('2013', '08', '01'),
+        cityCount: 33,
+        technologyCount: 8,
+        affinities: {
+            purity: 2,
+            supremacy: 3,
+            harmony: 7
+        }
+    }, {
+        description: 'A very bad game',
+        faction: 'Pan Asian Cooperative',
+        status: 'In Progress',
+        startdate: new Date('2013', '08', '01'),
+        cityCount: 6,
+        technologyCount: 10,
+        affinities: {
+            purity: 2,
+            supremacy: 3,
+            harmony: 7
+        }
+    }, {
+        description: 'I dont know',
+        faction: 'Chungsu',
+        status: 'In Progress',
+        startdate: new Date('2013', '08', '01'),
+        cityCount: 12,
+        technologyCount: 11,
+        affinities: {
+            purity: 2,
+            supremacy: 3,
+            harmony: 7
+        }
+    }];
 }]);
--- js/directives/gameInfo.js
+++ js/directives/gameInfo.js
@@ -1,23 +1,26 @@
 app.directive('gameInfo', function() {
-  return {
-    restrict: 'E',
-    scope: {
-      info: '='
-    },
-    templateUrl: 'js/directives/gameInfo.html',
-    link: function(scope, element, attrs) {
-      scope.buttonText = 'Button Text!';
-      scope.installed = false;
-      scope.download = function() {
-        element.toggleClass('btn-active');
-        if (scope.installed) {
-          scope.buttonText = 'Button Text 2!';
-          scope.installed = false;
-        } else {
-          scope.buttonText = 'Button Text 3!';
-          scope.installed = true;
+    return {
+        restrict: 'E',
+        scope: {
+            info: '='
+        },
+        templateUrl: 'js/directives/gameInfo.html',
+        link: function(scope, element, attrs) {
+            scope.incrementCity = function(adjustment) {
+                scope.info.cityCount += adjustment;
+            };
+            scope.incrementTechnology = function(adjustment) {
+                scope.info.technologyCount += adjustment;
+            };
+            scope.incrementPurity = function(adjustment) {
+                scope.info.affinities.purity += adjustment;
+            };
+            scope.incrementSupremacy = function(adjustment) {
+                scope.info.affinities.supremacy += adjustment;
+            };
+            scope.incrementHarmony = function(adjustment) {
+                scope.info.affinities.harmony += adjustment;
+            };
         }
-      }
     }
-  }
 });
