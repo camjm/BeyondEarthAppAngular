0901e5c04042ac8afec5400c45f7c06a6573115a
Cam <cam@mackay.net.nz>
2016-08-07 16:02:11 +1200

    more grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,39 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
-  // Load the task plugins
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-jade');
-  grunt.loadNpmTasks('grunt-contrib-stylus');
+    // Load the task plugins
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-contrib-stylus');
 
-  // Project configuration
-  grunt.initConfig({
-    clean: {
-      src: ['build']
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true
-      },
-      all: ['Gruntfile.js', 'src/**/*.{js,json}', 'test/**/*.js'],
-      json: "src/data/*.json"
-    },
-    // Compilation
-    jade: {
-      options: {
-        pretty: true
-      },
-      app: {
-        src: "src/views/app.jade",
-        dest: "build/app.html"
-      }
-    },
-    stylus: {
-      app: {
-        files: {
-          "build/css/app.css": "src/styles/app.styl"
+    // Project configuration
+    grunt.initConfig({
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true
+            },
+            all: ['Gruntfile.js', 'js/**/*.{js,json}']
+        },
+        // Compilation
+        jade: {
+            options: {
+                pretty: true
+            },
+            app: {
+                src: "src/views/app.jade",
+                dest: "index.html"
+            }
+        },
+        stylus: {
+            app: {
+                files: {
+                    "css/main.css": "src/styles/app.styl"
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
-  // Define the default task
-  grunt.registerTask('default', ['clean', 'jshint:all', 'jade', 'stylus']);
+    // Define the default task
+    grunt.registerTask('default', ['jshint', 'jade', 'stylus']);
 
 };
