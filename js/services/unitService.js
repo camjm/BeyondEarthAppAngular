52ae55c4e3a86dccdac70fa8b5893d6e736e1257
Cam <cam@mackay.net.nz>
2016-08-14 11:33:26 +1200

    use success/error callbacks instead of $promise

--- js/services/unitService.js
+++ js/services/unitService.js
@@ -1,5 +1,7 @@
 app.factory('unitService', ['$resource', function($resource) {
 
+    // Create resource object to interact with RESTful server-sdie data source.
+    // This provides high-level behaviours without the need to interact with the low level $http service.
     var unitResource = $resource('http://localhost:52204/api/v1/units/:id', {
         id: '@unitId'
     }, {
@@ -8,14 +10,18 @@ app.factory('unitService', ['$resource', function($resource) {
         }
     });
 
+    function errorHandler(httpResponse) {
+        console.log(httpResponse);
+    }
+
     return {
         getUnits: function(callback) {
-            unitResource.query().$promise.then(callback);
+            unitResource.query({}, callback, errorHandler);
         },
         getUnit: function(unitId, callback) {
             unitResource.get({
                 id: unitId
-            }).$promise.then(callback);
+            }, callback, errorHandler);
         },
         createUnit: function(unit, callback) {
             unitResource.save(null, unit).$promise.then(callback);
