369c0f54011332d7ee56513de7cf96c7f06500b3
Cam <cam@mackay.net.nz>
2016-09-03 19:59:08 +1200

    added bower and other grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,50 +1,77 @@
 module.exports = function(grunt) {
 
     // Load the task plugins
+    grunt.loadNpmTasks('grunt-bower');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-jade');
     grunt.loadNpmTasks('grunt-contrib-coffee');
     grunt.loadNpmTasks('grunt-contrib-stylus');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // Project configuration
     grunt.initConfig({
-        jshint: {
-            options: {
-                curly: true,
-                eqeqeq: true
-            },
-            grunt: "Gruntfile.js"
-        },
-        clean: {
-            src: ['build']
-        },
-        // Compilation
-        jade: {
-            options: {
-                pretty: true
-            },
-            app: {
-                src: "src/views/app.jade",
-                dest: "build/index.html"
-            }
-        },
-        coffee: {
-
-        },
-        stylus: {
-            options: {
-                compress: false
-            },
-            app: {
-                files: {
-                    "build/css/main.css": "src/styles/app.styl"
-                }
-            }
+      pkg: grunt.file.readJSON('package.json'),
+      banner:
+        '/*! <%= pkg.name %> -v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '* <%= pkg.homepage %>\n' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %> */\n',
+      jshint: {
+          options: {
+              curly: true,
+              eqeqeq: true
+          },
+          grunt: "Gruntfile.js",
+          test: "test/**/*.js"
+      },
+      clean: {
+          src: ['build']
+      },
+      bower: {
+        dev: {
+          dest: 'build'
         }
+      },
+      // Compilation
+      jade: {
+          options: {
+              pretty: true
+          },
+          app: {
+              src: "src/views/app.jade",
+              dest: "build/index.html"
+          }
+      },
+      coffee: {
+        options: {
+          sourceMap: true
+        },
+        files: {}
+      },
+      stylus: {
+          options: {
+              compress: false
+          },
+          app: {
+              files: {
+                  "build/css/main.css": "src/styles/app.styl"
+              }
+          }
+      },
+      uglify: {
+        options: {
+          banner: '<%= banner %>',
+          sourceMap: true,
+          sourceMapIn: '<%= coffee.files.dest %>.map'
+        },
+        build: {
+          src: 'build/something.js',
+          build: 'build/something.min.js'
+        }
+      }
     });
 
     // Define the default task
-    grunt.registerTask('default', ['jshint:grunt', 'clean', 'jade', 'coffee', 'stylus']);
+    grunt.registerTask('default', ['jshint', 'clean', 'bower', 'jade', 'stylus', 'uglify']);
 
 };
